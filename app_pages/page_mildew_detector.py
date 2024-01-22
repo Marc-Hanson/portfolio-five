1f93876c1c5fc9ead002417eed56bf64b26ff50d
Marc-Hanson <>
2024-01-22 19:04:37 +0000

    Replaced .append with ._append in src files.

--- app_pages/page_mildew_detector.py
+++ app_pages/page_mildew_detector.py
@@ -43,7 +43,7 @@ def page_mildew_detector_body():
                 resized_img, version=version)
             plot_predictions_probabilities(pred_proba, pred_class)
 
-            df_report = df_report.append({"Name": image.name, 'Result': pred_class},
+            df_report = df_report._append({"Name": image.name, 'Result': pred_class},
                                          ignore_index=True)
 
         if not df_report.empty:
